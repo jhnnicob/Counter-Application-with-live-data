18ac52f1180fe97b8aecb7d70e92c3666602d817
Nico <johnnicomolon@yahoo.com>
2021-09-27 00:20:52 -0400

    Using LiveData

--- app/src/main/java/com/nico/counterapplicationwithlivedata/CounterFragment.kt
+++ app/src/main/java/com/nico/counterapplicationwithlivedata/CounterFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import com.nico.counterapplicationwithlivedata.databinding.FragmentCounterBinding
@@ -13,6 +14,7 @@ class CounterFragment : Fragment() {
 
     private var _binding: FragmentCounterBinding? = null
     private val binding get() = _binding!!
+    private var counter: MutableLiveData<Int> = MutableLiveData(0)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,8 +29,8 @@ class CounterFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        var counter: MutableLiveData<Int> = MutableLiveData(0)
-        counter.observe(viewLifecycleOwner, Observer {
+
+        counter().observe(viewLifecycleOwner, Observer {
             binding.textviewCounter.text = counter.value.toString()
         })
 
@@ -37,6 +39,10 @@ class CounterFragment : Fragment() {
                 counter.value = it + 1
             }
         }
+    }
+
+    private fun counter() : LiveData<Int> {
+        return counter
     }
 
     override fun onDestroyView() {
